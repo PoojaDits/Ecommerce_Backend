95ce58921d164804770f3a2fda91b97b021024c2
PoojaDits <poojajoshiwins@outlook.com>
2026-05-28 11:25:52 +0530

    update categories

--- src/entities/Category.ts
+++ src/entities/Category.ts
@@ -1,13 +1,6 @@
-import { 
-    Column, 
-    Entity, 
-    OneToMany, 
-    PrimaryGeneratedColumn, 
-    BeforeInsert, 
-    BeforeUpdate 
-} from "typeorm";
-import slugify from "slugify";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import Product from "./Product";
+
 
 @Entity("categories")
 export default class Category {
@@ -15,29 +8,12 @@ export default class Category {
     @PrimaryGeneratedColumn("increment")
     id: number;
 
-    @Column({ type: "varchar", unique: true })
+    @Column({type:"varchar", unique: true})
     name: string;
-
-    // Added slug column
-    @Column({ type: "varchar", unique: true })
-    slug: string;
-
-    @Column({ type: "text", nullable: true })
+    @Column({type:"text", nullable:true})
     description: string;
 
-    @OneToMany(() => Product, (product) => product.category)
+    @OneToMany(()=>Product,(product)=>product.category)
     products: Product[];
 
-    // Auto-generates slug before saving
-    @BeforeInsert()
-    @BeforeUpdate()
-    generateSlug() {
-        if (this.name) {
-            this.slug = slugify(this.name, {
-                lower: true,      
-                strict: true,     
-                trim: true        
-            });
-        }
-    }
-}
+}   
